d86ca99a87ad3a8280716d33ce34b47c05a476f0
arnaldoxpm <arnaldoquintero96@gmail.com>
2017-01-26 13:34:33 -0500

    Sin clase Servicio

--- servicios.py
+++ servicios.py
@@ -11,160 +11,158 @@ class Tarifa:
         self.tarifaSemana = semana
         self.tarifaFinSemana = finSemana
 
-class Servicio:
+def calcularMonto(horas: int, cobro: float)->int:
+    #print ("Monto a Pagar: " + horas*cobro)
+    return horas*cobro
 
-    def calcularMonto(horas: int, cobro: float)->int:
-        #print ("Monto a Pagar: " + horas*cobro)
-        return horas*cobro
+def calcularPrecio(tarifa: Tarifa, tiempoServicio: [datetime.datetime]) -> float:
+    horaF = datetime.time(23,59,59,999)
+    inicio = tiempoServicio[0]
+    fin = tiempoServicio[1]
 
-    def calcularPrecio(tarifa: Tarifa, tiempoServicio: [datetime.datetime]) -> float:
-        horaF = datetime.time(23,59,59,999)
-        inicio = tiempoServicio[0]
-        fin = tiempoServicio[1]
+    if (inicio.month != fin.month):
 
-        if (inicio.month != fin.month):
+        #para el mes del inicio
+        horasi = 24 - inicio.hour
+        sumai=0
+        temp = inicio.weekday()
 
-            #para el mes del inicio
-            horasi = 24 - inicio.hour
-            sumai=0
-            temp = inicio.weekday()
+        if (inicio.month in (1,3,5,7,8,10,12)):
+            i= 31 - inicio.day
 
-            if (inicio.month in (1,3,5,7,8,10,12)):
-                i= 31 - inicio.day
-
-            elif (inicio.month==2):
-                if (inicio.year%4 == 0):
-                    i = 29 - inicio.day
-                else:
-                    i = 28 - inicio.day
+        elif (inicio.month==2):
+            if (inicio.year%4 == 0):
+                i = 29 - inicio.day
             else:
-                i = 30 - inicio.day
+                i = 28 - inicio.day
+        else:
+            i = 30 - inicio.day
 
-            while i-1 > 0 and i<=7:
-                temp+=1
-                if (temp<5):
-                    cobrot = tarifa.tarifaSemana
-                else:
-                    cobrot = tarifa.tarifaFinSemana
-                sumai += Servicio.calcularMonto(24,cobrot)
-                i-=1
-
-            if (i>7):
-                return "ERROR"
-
-
-            #para el mes de fin
-            if ((fin.minute - inicio.minute)>0):
-                horasf = fin.hour
+        while i-1 > 0 and i<=7:
+            temp+=1
+            if (temp<5):
+                cobrot = tarifa.tarifaSemana
             else:
-                horasf = fin.hour - 1
+                cobrot = tarifa.tarifaFinSemana
+            sumai += calcularMonto(24,cobrot)
+            i-=1
 
-            j = fin.day
-            sumaf = 0
-            temp = fin.weekday()
-            while j-1 > 0 and i<=7:
-                temp-=1
-                if (temp<5):
-                    cobrot = tarifa.tarifaSemana
+        if (i>7):
+            return "ERROR"
+
+
+        #para el mes de fin
+        if ((fin.minute - inicio.minute)>0):
+            horasf = fin.hour
+        else:
+            horasf = fin.hour - 1
+
+        j = fin.day
+        sumaf = 0
+        temp = fin.weekday()
+        while j-1 > 0 and i<=7:
+            temp-=1
+            if (temp<5):
+                cobrot = tarifa.tarifaSemana
+            else:
+                cobrot = tarifa.tarifaFinSemana
+            sumaf += calcularMonto(24,cobrot)
+            i-=1
+
+        if (i>7):
+            return "ERROR"
+
+        return horasf+horasi+sumai+sumaf
+
+
+    else:
+        if (fin.day == inicio.day):
+
+            if (fin.hour != inicio.hour):
+
+                horas  = fin.hour - inicio.hour - 1
+
+                if ((fin.minute - inicio.minute)>=0):
+                    minutos = fin.minute - inicio.minute
                 else:
-                    cobrot = tarifa.tarifaFinSemana
-                sumaf += Servicio.calcularMonto(24,cobrot)
-                i-=1
+                    minutos = 60 - abs(fin.minute - inicio.minute)
 
-            if (i>7):
-                return "ERROR"
+                tiempo = horas + math.ceil(minutos/60)
 
-            return horasf+horasi+sumai+sumaf
+            else:
 
+                minutos = fin.minute - inicio.minute
+                if (minutos>=15):
+                    tiempo = 1
+                else:
+                    tiempo = 0
+
+            if (fin.weekday()<5):
+                cobro = tarifa.tarifaSemana
+            else:
+                cobro = tarifa.tarifaFinSemana
+
+            return calcularMonto(tiempo,cobro)
 
         else:
-            if (fin.day == inicio.day):
 
-                if (fin.hour != inicio.hour):
+            if(fin.day - inicio.day == 1):
 
-                    horas  = fin.hour - inicio.hour - 1
-
-                    if ((fin.minute - inicio.minute)>=0):
-                        minutos = fin.minute - inicio.minute
-                    else:
-                        minutos = 60 - abs(fin.minute - inicio.minute)
-
-                    tiempo = horas + math.ceil(minutos/60)
-
+                horasi = 24 - inicio.hour
+                if ((fin.minute - inicio.minute)>0):
+                    horasf = fin.hour
                 else:
-
-                    minutos = fin.minute - inicio.minute
-                    if (minutos>=15):
-                        tiempo = 1
-                    else:
-                        tiempo = 0
+                    horasf = fin.hour - 1
 
                 if (fin.weekday()<5):
-                    cobro = tarifa.tarifaSemana
+                    cobrof = tarifa.tarifaSemana
                 else:
-                    cobro = tarifa.tarifaFinSemana
+                    cobrof = tarifa.tarifaFinSemana
 
-                return Servicio.calcularMonto(tiempo,cobro)
+                if (inicio.weekday()<5):
+                    cobroi = tarifa.tarifaSemana
+                else:
+                    cobroi = tarifa.tarifaFinSemana
+
+                return calcularMonto(horasf,cobrof)+calcularMonto(horasi,cobroi)
 
             else:
 
-                if(fin.day - inicio.day == 1):
-
-                    horasi = 24 - inicio.hour
-                    if ((fin.minute - inicio.minute)>0):
-                        horasf = fin.hour
-                    else:
-                        horasf = fin.hour - 1
-
-                    if (fin.weekday()<5):
-                        cobrof = tarifa.tarifaSemana
-                    else:
-                        cobrof = tarifa.tarifaFinSemana
-
-                    if (inicio.weekday()<5):
-                        cobroi = tarifa.tarifaSemana
-                    else:
-                        cobroi = tarifa.tarifaFinSemana
-
-                    return Servicio.calcularMonto(horasf,cobrof)+Servicio.calcularMonto(horasi,cobroi)
-
+                horasi = 24 - inicio.hour
+                if ((fin.minute - inicio.minute)>0):
+                    horasf = fin.hour
                 else:
+                    horasf = fin.hour - 1
 
-                    horasi = 24 - inicio.hour
-                    if ((fin.minute - inicio.minute)>0):
-                        horasf = fin.hour
+                if (fin.weekday()<5):
+                    cobrof = tarifa.tarifaSemana
+                else:
+                    cobrof = tarifa.tarifaFinSemana
+
+                if (inicio.weekday()<5):
+                    cobroi = tarifa.tarifaSemana
+                else:
+                    cobroi = tarifa.tarifaFinSemana
+
+                i = fin.day - inicio.day
+                suma=0
+                temp = inicio.weekday()
+                while i-1 > 0 and i<=7:
+                    temp += 1
+                    if (temp<5):
+                        cobrot = tarifa.tarifaSemana
                     else:
-                        horasf = fin.hour - 1
+                        cobrot = tarifa.tarifaFinSemana
+                    suma += calcularMonto(24,cobrot)
+                    i-=1
 
-                    if (fin.weekday()<5):
-                        cobrof = tarifa.tarifaSemana
-                    else:
-                        cobrof = tarifa.tarifaFinSemana
+                if (i>7):
+                    return "ERROR"
 
-                    if (inicio.weekday()<5):
-                        cobroi = tarifa.tarifaSemana
-                    else:
-                        cobroi = tarifa.tarifaFinSemana
-
-                    i = fin.day - inicio.day
-                    suma=0
-                    temp = inicio.weekday()
-                    while i-1 > 0 and i<=7:
-                        temp += 1
-                        if (temp<5):
-                            cobrot = tarifa.tarifaSemana
-                        else:
-                            cobrot = tarifa.tarifaFinSemana
-                        suma += Servicio.calcularMonto(24,cobrot)
-                        i-=1
-
-                    if (i>7):
-                        return "ERROR"
-
-                    return suma+Servicio.calcularMonto(horasf,cobrof)+Servicio.calcularMonto(horasi,cobroi)
+                return suma+calcularMonto(horasf,cobrof)+calcularMonto(horasi,cobroi)
 
 
 #i = datetime.datetime(2017,1,22,10,45)
 #j = datetime.datetime(2017,1,23,12,40)
 #tarifa = Tarifa(10,100)
-#print (Servicio.calcularPrecio(tarifa,[i,j]))
+#print (calcularPrecio(tarifa,[i,j]))
